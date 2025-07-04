632cbb7ca9b083c4ffd24418a271a879621b19fd
Ajatshatrusingh <152709095+ajatshatrusingh23@users.noreply.github.com>
2025-07-04 10:19:58 +0530

    Time: 0 ms (100%), Space: 10.5 MB (67.96%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void func(vector<int> &nums,int start,vector<int> &temp,vector<vector<int>> &res){
-        sort(nums.begin(),nums.end());
+         
         res.push_back(temp);
         
         for(int i = start;i<nums.size();i++){
@@ -12,6 +12,7 @@ public:
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         vector<int>temp;
         func(nums,0,temp,ans);
