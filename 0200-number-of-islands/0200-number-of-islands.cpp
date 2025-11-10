590f05b5b4bc7879d1d6087d90465d92bd124692
Ajatshatrusingh <152709095+ajatshatrusingh23@users.noreply.github.com>
2025-11-10 14:38:47 +0530

    Time: 20 ms (93.94%), Space: 16.3 MB (78.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void DFS(vector<vector<char>>& grid,int i,int j,int r,int c){
-        if(i == r || j == c || i < 0|| j <0|| grid[i][j] == '0'){
+        if(i == r || j == c || i< 0|| j <0|| grid[i][j] == '0'){
             return;
         }
         grid[i][j] = '0';
@@ -20,12 +20,12 @@ public:
 
         for(int i =0;i<r;i++){
             for(int j = 0;j<c;j++){
-                {
+                
                     if(grid[i][j] =='1'){
                         DFS(grid,i,j,r,c);
                             ans++;
                     }
-                }
+                
             }
         }
 
