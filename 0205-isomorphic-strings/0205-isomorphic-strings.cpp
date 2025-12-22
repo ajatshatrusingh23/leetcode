263773ab8d0c3cccb269c53f5030cf9142b9efcf
Ajatshatrusingh <152709095+ajatshatrusingh23@users.noreply.github.com>
2025-12-22 23:04:36 +0530

    Time: 4 ms (22.72%), Space: 9.4 MB (27.96%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length())
+        return false;
+
+    unordered_map<char, char> mapST;  
+    unordered_map<char, char> mapTS;  
+
+    for (int i = 0; i < s.length(); i++) {
+        char chS = s[i];
+        char chT = t[i];
+
+        
+        if (mapST.count(chS)) {
+            if (mapST[chS] != chT) {
+                return false; 
+            }
+        } else {
+            mapST[chS] = chT;  
+        }
+
+        
+        if (mapTS.count(chT)) {
+            if (mapTS[chT] != chS) {
+                return false;  
+            }
+        } else {
+            mapTS[chT] = chS;  
+        }
+    }
+
+    return true;
+    }
+};
