8134746614c7351cc1180ecec6b7f6526f074fd0
Ajatshatrusingh <152709095+ajatshatrusingh23@users.noreply.github.com>
2026-02-05 14:17:04 +0530

    Time: 0 ms (100%), Space: 15.2 MB (6.24%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -4,19 +4,17 @@ public:
         int n = nums.size();
 
         vector<int>dp(n,1);
-        int ans = 1;
 
-        for(int i =1;i<n;i++){
-            if(nums[i] > nums[i-1]){
-                dp[i] = dp[i-1]+1;
-               
+        for(int i = 1;i<n;i++){
+            if(nums[i-1] < nums[i]){
+                dp[i] = max(dp[i],dp[i-1] + 1);
             }
             else{
                 dp[i] = 1;
             }
-            ans = max(ans,dp[i]);
 
         }
-        return ans;
+
+        return *max_element(dp.begin(),dp.end());
     }
 };
