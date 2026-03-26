ebab646b230cb2945d00312698d9d7439a5921b3
Ajatshatrusingh <152709095+ajatshatrusingh23@users.noreply.github.com>
2026-03-26 17:48:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -15,20 +15,29 @@ public:
         vector<int> ans;
 
         if(!root) return ans;
-        queue<TreeNode*> q;
+
+        queue<TreeNode*>q;
+
         q.push(root);
+
         while(!q.empty()){
             int size = q.size();
-            for(int i = 0;i<size;i++){
+
+            for(int i= 0;i<size;i++){
                 TreeNode* curr = q.front();
                 q.pop();
-                 
-                if(curr->left) q.push(curr->left);
+
+                if(curr -> left)q.push(curr->left);
                 if(curr->right) q.push(curr->right);
                 if(i == size-1) ans.push_back(curr->val);
+
+
+
             }
 
+
         }
+
         return ans;
     }
 };
