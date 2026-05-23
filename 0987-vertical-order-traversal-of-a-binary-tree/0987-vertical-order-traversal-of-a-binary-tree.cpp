f46ed8ab8337315ad59f6ce564b056c89005840f
Ajatshatrusingh <152709095+ajatshatrusingh23@users.noreply.github.com>
2026-05-23 12:11:20 +0530

    Time: 0 ms (100%), Space: 15.4 MB (92.47%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,31 +11,39 @@
  */
 class Solution {
 public:
-
-    map<int, vector<pair<int,int>>>coltable;
-
-    void inorder(TreeNode* root,int row, int col){
-        if(root == NULL) return;
-
-        inorder(root->left,row+1,col-1);
-
-        coltable[col].push_back({row,root->val});
-
-        inorder(root->right,row+1,col+1);
-    }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        inorder(root,0,0);
+        map<int,vector<pair<int,int>>>coltabel;
+
+        queue<pair<TreeNode*,pair<int,int>>>q;
+
+        q.push({root,{0,0}});
+
+        while(!q.empty()){
+            auto front = q.front();
+            q.pop();
+
+            TreeNode* node = front.first;
+
+            int row = front.second.first;
+            int col = front.second.second;
+
+            coltabel[col].push_back({row,node->val});
+
+            if(node->left) q.push({node->left,{row+1,col-1}});
+
+            if(node->right) q.push({node->right,{row+1,col+1}});
+
+
+        }
 
         vector<vector<int>>result;
 
-        for(auto &[col,nodes]: coltable){
-
+        for(auto &[col,nodes]: coltabel){
             sort(nodes.begin(),nodes.end());
 
             vector<int>temp;
 
             for(auto &[row,val]:nodes){
-
                 temp.push_back(val);
             }
 
@@ -43,6 +51,7 @@ public:
         }
 
         return result;
+
 
 
 
